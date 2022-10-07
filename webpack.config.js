50799fd5da00641561ad38b21291e7f95d256966
Jon Rickard Yxelflod <rickard.yxelflod@dragster.se>
2022-10-07 17:07:34 +0200

    add css, scss, postcss, hot, browserslistrc

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,27 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 let mode = 'development';
+let target = 'web';
 
 if (process.env.NODE_ENV === 'production') {
 	mode = 'production';
+  target = 'browserslist';
 }
 
 module.exports = {
 	mode: mode,
+  target: target,
 	module: {
 		rules: [
+			{
+				test: /\.(s[ac]|c)ss$/i,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'postcss-loader',
+					'sass-loader',
+				],
+			},
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
@@ -20,6 +33,8 @@ module.exports = {
 	},
 	devServer: {
 		static: path.join(__dirname, 'dist'),
+		hot: true,
 	},
+	plugins: [new MiniCssExtractPlugin()],
 	devtool: 'source-map',
 };
