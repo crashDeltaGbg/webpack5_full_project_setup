08573fdbe5a8bb5f65caefbc2afacbe2bb8e4217
Jon Rickard Yxelflod <rickard.yxelflod@dragster.se>
2022-10-08 13:06:20 +0200

    add image support to webpack

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import "../styles/index.scss";
 import Recipes from "./Recipes";
+import guitar from "../images/sarah-arista-n7XS9AbpZ5w-unsplash.jpg";
+import guitarSvg from "../images/1F3B8.svg";
 
 import React from "react";
 
@@ -10,9 +12,11 @@ const App = () => {
       <main>
         <section>
           <h1>Oh hai, React</h1>
+          <img src={guitar} alt="Guitar" width="250" />
+          <img src={guitarSvg} alt="Guitar" width="250" />
         </section>
+        <Recipes />
       </main>
-      <Recipes/>
     </>
   );
 };
