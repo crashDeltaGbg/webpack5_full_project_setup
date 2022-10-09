f95c87daf58e8a140d62f2cc8b8ef7701a572f3b
Jon Rickard Yxelflod <rickard.yxelflod@dragster.se>
2022-10-09 21:35:10 +0200

    add react refresh for develompent

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,17 @@
+const plugins = [];
+
+function checkClass(pluginClass) {
+  return pluginClass != "react-refresh/babel";
+}
+
+if (process.env.NODE_ENV !== "production") {
+  plugins.push("react-refresh/babel");
+}
+
 module.exports = {
   presets: [
     "@babel/preset-env",
     ["@babel/preset-react", { runtime: "automatic" }],
   ],
+  plugins: plugins,
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,34 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const path = require("path");
+
 let mode = "development";
 let target = "web";
+
+const plugins = [
+  new CleanWebpackPlugin(),
+  new MiniCssExtractPlugin(),
+  new HtmlWebpackPlugin({
+    template: "./src/index.html",
+  }),
+];
+
+console.log(plugins);
 
 if (process.env.NODE_ENV === "production") {
   mode = "production";
   target = "browserslist";
+} else {
+  plugins.push(new ReactRefreshWebpackPlugin());
 }
 
 module.exports = {
   mode: mode,
   target: target,
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     assetModuleFilename: "images/[name][ext][query]",
@@ -49,13 +64,7 @@ module.exports = {
     static: path.join(__dirname, "dist"),
     hot: true,
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
+  plugins: plugins,
   resolve: {
     extensions: [".js", ".jsx"],
   },
